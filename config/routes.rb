e9b537f2b9f829973c653836aed59532bea8ea2f
Lorry Rocha <sorai120@yahoo.com>
2013-12-16 13:19:42 -0500

    Revert "Added all the thingies in the threemonths view. Found bug where none of the big decimals are showing because they were inputted wrong. Going to fix with a migration"
    This reverts commit c279b2606574c40f27bd6a8d55056c0d2e628ce7.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Korning::Application.routes.draw do
   resources :employees, only: [:index, :show]
   resources :customers, only: [:index, :show]
   resources :products, only: [:index, :show]
-  resources :threemonths, only: [:index, :show]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
